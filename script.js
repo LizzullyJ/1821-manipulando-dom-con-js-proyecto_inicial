e577635a3c277bbbdcc2f49f962c2e9f0b81078e
Lizzully <lizzullytha@gmail.com>
2023-07-01 21:28:57 -0600

    clase 5 completada

--- script.js
+++ script.js
@@ -1,47 +1,36 @@
-(()=>{
-const btn = document.querySelector('[data-form-btn]');
-
-//listener click
-//arrow functions o funciones anonimas
-const createTask = (evento) => {
-  
-    evento.preventDefault();
-    const input = document.querySelector('[data-form-input]');
-    const value = input.value;
-    const list = document.querySelector('[data-list]');
-    const task = document.createElement('li');
-    task.classList.add('card');
-    input.value = '';
-    //backticks
-
-    const taskContent = document.createElement('div');
-    const tiltleTask = document.createElement('span');
-    tiltleTask.classList.add("task");
-    tiltleTask.innerText = value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(tiltleTask);
-    const content = `  
-   <i class = 'fas fa-trash-alt trashIcon icon'></i> `;
- // task.innerHTML = content;
- task.appendChild(taskContent);
-  list.appendChild(task);
-}
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+(() => {
 
 
-btn.addEventListener("click", createTask);
+    const btn = document.querySelector('[data-form-btn]');
 
-const checkComplete = () => {
-    const i = document.createElement('i');
-    i.classList.add('far', 'fa-check-square', 'icon');
-    i.addEventListener('click', completeTask);
-     return i;
-};
+    //listener click
+    //arrow functions o funciones anonimas
+    const createTask = (evento) => {
 
-const completeTask = (event)=>{
-   
-    const element = event.target;
-    element.classList.toggle('fas');   
-    element.classList.toggle('far');
-    element.classList.toggle('completeIcon');
-};
-}) ();
+        evento.preventDefault();
+        const input = document.querySelector('[data-form-input]');
+        const value = input.value;
+        const list = document.querySelector('[data-list]');
+        const task = document.createElement('li');
+        task.classList.add('card');
+        input.value = '';
+        //backticks
+
+        const taskContent = document.createElement('div');
+        const tiltleTask = document.createElement('span');
+        tiltleTask.classList.add("task");
+        tiltleTask.innerText = value;
+        taskContent.appendChild(checkComplete());
+        taskContent.appendChild(tiltleTask);
+        
+      
+        task.appendChild(taskContent);
+        task.appendChild(deleteIcon());
+        list.appendChild(task);
+    }
+
+    btn.addEventListener("click", createTask);
+
+})();
