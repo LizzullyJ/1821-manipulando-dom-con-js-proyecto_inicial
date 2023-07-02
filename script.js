4ba2229480539098ada017d272023971623e44a2
Lizzully <lizzullytha@gmail.com>
2023-07-01 19:24:06 -0600

    clase 03 complatada

--- script.js
+++ script.js
@@ -5,11 +5,23 @@ console.log(btn);
 
 //listener click
 //arrow functions o funciones anonimas
-const createTask =  (evento) =>{
+const createTask = (evento) => {
     console.log(evento);
     evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add('card');
+    input.value = "";
+    const content = ` <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>`;
+  task.innerHTML = content;
+  list.appendChild(task);
+
+    console.log(content);
 }
 
 
